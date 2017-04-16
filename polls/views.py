cc69f5c49ef0269bd6f154d7f337fccc10b43fb5
ryo-1 <f1242022@kgu.jp>
2017-04-16 19:02:57 +0900

    プロジェクト作成

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'polls/index.html',{'ans':'お試し'})
