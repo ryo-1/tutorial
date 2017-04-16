69289033ff94bbc53e197d70a4f27f1c3c4c7f40
ryo-1 <f1242022@kgu.jp>
2017-04-16 23:26:06 +0900

    修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,3 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
