9db3a834fcfe04ea761c64a4de8a9725d9af5c9f
ryo-1 <f1242022@kgu.jp>
2017-04-19 00:18:35 +0900

    test

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,11 @@ from django.template import loader
 
 def index(request):
     return render(request, 'polls/index.html',{'ans':'お試し'})
+
+def manval(request):
+    try:
+        text=request.POST['manval']
+    except (KeyError):
+        return render(request, 'polls/index.html',{'ans':'失敗'})
+    else:
+        return render(request, 'polls/index.html',{'ans':'成功'+text})
